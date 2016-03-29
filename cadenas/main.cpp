cbd7090ee6b744416616157469318153357c82d9
CanalesMaria <canales.aguilar@unitec.edu>
2016-03-29 16:45:08 -0500

    Programas 2
    programas del segundo parcial

---
+++ cadenas/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+using namespace std;
+char nombre[5][30];
+void ingresocadenas(char nombre[5][30])
+char cadcompa[30];
+cout<<"ingresar cadena a comparar";
+cine.getline(cadcompa,30);
+
